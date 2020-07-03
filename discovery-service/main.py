6e4d499415f3e084343eceaa3a61eb3112270ecb
Viacheslav Kovalevskyi <vkovalevskyi@google.com>
2020-07-02 18:01:43 -0700

    updated discovery service

--- discovery-service/main.py
+++ discovery-service/main.py
@@ -20,37 +20,62 @@ newsapi = NewsApiClient(api_key=news_api_key)
 
 
 def process_function_call(event, context):
+    publish_top_headlights_to_pipeline()
     publish_latest_news_to_pipeline()
 
 
+def publish_url(url):
+    msg_dict = {
+        "url": url,
+        "test": True
+    }
+    msg_str = json.dumps(msg_dict)
+    print("msg to post: {}".format(msg_str))
+    msg_data = msg_str.encode("utf-8")
+    publisher.publish(
+        topic_path, msg_data
+    )
+    print("published")
+    # FIXME: On average it takes 30 seconds to process new URL
+    # BERT is the bottle neck.
+    time.sleep(10)
+
+
 def publish_latest_news_to_pipeline():
+    sources = newsapi.get_sources(category="technology", language="en", country="us")
+    # print(type(sources))
+    # print(sources)
+    sources = [source["id"] for source in sources["sources"]]
+    sources_str = ",".join(sources)
+    print("sources: {}".format(sources_str))
+    yesterday = date.today() - timedelta(days=1)
+    top_headlines = newsapi.get_everything(from_param=yesterday,
+                                           language="en",
+                                           sort_by="relevancy",
+                                           sources=sources_str,
+                                           page_size=20)
+    for headline in top_headlines["articles"]:
+        print(headline["title"])
+        print(headline["url"])
+        publish_url(headline["url"])
+
+
+def publish_top_headlights_to_pipeline():
     yesterday = date.today() - timedelta(days=1)
     # /v2/top-headlines
     top_headlines = newsapi.get_top_headlines(category="technology",
                                               language="en",
-                                              country="us")
-
+                                              country="us",
+                                              page_size=100)
+    print(str(top_headlines))
     for headline in top_headlines["articles"]:
         headline_date = dateutil.parser.parse(headline["publishedAt"]).date()
         if yesterday < headline_date:
             print(headline["title"])
             print(headline["url"])
-            url = headline["url"]
-            msg_dict = {
-                "url": url,
-                "test": True
-            }
-            msg_str = json.dumps(msg_dict)
-            print("msg to post: {}".format(msg_str))
-            msg_data = msg_str.encode("utf-8")
-            publisher.publish(
-                topic_path, msg_data
-            )
-            print("published")
-            # FIXME: On average it takes 30 seconds to process new URL
-            # BERT is the bottle neck.
-            time.sleep(10)
+            publish_url(headline["url"])
 
 
 if "__main__" == __name__:
     publish_latest_news_to_pipeline()
+    publish_top_headlights_to_pipeline()
